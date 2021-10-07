c28ef99cdd1f26dbe1359f2ff43898adb43691f5
Arjun Surendra <srao.arjun@gmail.com>
2021-10-07 15:26:33 +0530

    Temp DB Monitoring Script

---
+++ SQL/TEMP_DB_Monitoring.sql
@@ -0,0 +1,42 @@
+
+-- Determining the amount of free space in tempdb
+SELECT SUM(unallocated_extent_page_count) AS [free pages],
+  (SUM(unallocated_extent_page_count)*1.0/128) AS [free space in MB]
+FROM tempdb.sys.dm_db_file_space_usage;
+
+-- Determining the amount of space used by the version store
+SELECT SUM(version_store_reserved_page_count) AS [version store pages used],
+  (SUM(version_store_reserved_page_count)*1.0/128) AS [version store space in MB]
+FROM tempdb.sys.dm_db_file_space_usage;
+
+-- Determining the amount of space used by internal objects
+SELECT SUM(internal_object_reserved_page_count) AS [internal object pages used],
+  (SUM(internal_object_reserved_page_count)*1.0/128) AS [internal object space in MB]
+FROM tempdb.sys.dm_db_file_space_usage;
+
+-- Determining the amount of space used by user objects
+SELECT SUM(user_object_reserved_page_count) AS [user object pages used],
+  (SUM(user_object_reserved_page_count)*1.0/128) AS [user object space in MB]
+FROM tempdb.sys.dm_db_file_space_usage;
+
+GO
+
+-- Obtaining the space consumed by internal objects in all currently running tasks in each session
+SELECT session_id,
+  SUM(internal_objects_alloc_page_count) AS task_internal_objects_alloc_page_count,
+  SUM(internal_objects_dealloc_page_count) AS task_internal_objects_dealloc_page_count
+FROM sys.dm_db_task_space_usage
+where session_id = 80--FILTER BY A SESSION ID IF WE NEED TO CHECK FOR ONE PARTICULAR SESSION ID THAT IS A BOTTLENECK
+GROUP BY session_id;
+
+-- Obtaining the space consumed by internal objects in the current session for both running and completed tasks
+SELECT R2.session_id,
+  R1.internal_objects_alloc_page_count
+  + SUM(R2.internal_objects_alloc_page_count) AS session_internal_objects_alloc_page_count,
+  R1.internal_objects_dealloc_page_count
+  + SUM(R2.internal_objects_dealloc_page_count) AS session_internal_objects_dealloc_page_count
+FROM sys.dm_db_session_space_usage AS R1
+INNER JOIN sys.dm_db_task_space_usage AS R2 ON R1.session_id = R2.session_id
+where R2.session_id = 80--FILTER BY A SESSION ID IF WE NEED TO CHECK FOR ONE PARTICULAR SESSION ID THAT IS A BOTTLENECK 
+GROUP BY R2.session_id, R1.internal_objects_alloc_page_count,
+  R1.internal_objects_dealloc_page_count;
