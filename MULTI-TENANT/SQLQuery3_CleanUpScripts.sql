b6a30fb56bf92a3d5810dfd42f6b81e997140d7f
Arjun Surendra <srao.arjun@gmail.com>
2021-10-19 16:16:54 +0530

    Drop Scripts Updated

--- MULTI-TENANT/SQLQuery3_CleanUpScripts.sql
+++ MULTI-TENANT/SQLQuery3_CleanUpScripts.sql
@@ -1,23 +1,32 @@
+USE [MultiTenantDB]
+GO
+
 /*
 Clean up scripts to drop all the relavent objects and their dependencies 
 */
 
 -- Drop the Procedures
-DROP PROCEDURE IF EXISTS Site_01.sp_select_employees
+DROP PROCEDURE IF EXISTS Site_01.sp_select_Equipments
 GO
-DROP PROCEDURE IF EXISTS Site_02.sp_select_employees
+DROP PROCEDURE IF EXISTS Site_02.sp_select_Equipments
+GO
+DROP PROCEDURE IF EXISTS sch_cmn.sp_select_Configuration
 GO
 
 --Drop the tables 
-Drop table if exists Site_01.employees 
+Drop TABLE IF EXISTS Site_01.Equipments
 GO
-Drop table if exists Site_02.employees 
+Drop TABLE IF EXISTS Site_02.Equipments 
+GO
+DROP TABLE IF EXISTS [sch_cmn].[tblConfiguration]
 GO
 
 --Drop the Schemas
 DROP SCHEMA IF EXISTS [Site_01]
 GO
 DROP SCHEMA IF EXISTS  [Site_02]
+GO
+DROP SCHEMA IF EXISTS [sch_cmn]
 GO
 
 --- Drop the Users
