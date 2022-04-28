ae66dac23f20c92a413b8c42d1039c2092e37e2d
Arjun Surendra <srao.arjun@gmail.com>
2022-04-28 20:16:45 +0530

    Changed the logic to include Purging override capabalities and take values from Global configuration - LookUP Table .
    In case there is a value in the archival config it should override the
    value from the global config for the paramters
    
    "batch_size" and "history_data_retention_days"

--- PURGE DEMO/7_Sp_Arc_RunArchiving.sql
+++ PURGE DEMO/7_Sp_Arc_RunArchiving.sql
@@ -1,3 +1,5 @@
+USE [Honeywell.MES.Operations.DataModel.OperationsDB]
+GO
 /*
 
 begin tran
@@ -18,6 +20,7 @@ declare @destination_database_name varchar(200)
 declare @tablename varchar(500)  
 declare @schemaname varchar(100)
 declare @batchsize int 
+declare @override_history_data_retention_days int
 declare @archival_config_id int 
 declare @tablename_suffix varchar(100)= ''
 declare @purgeOnly bit
@@ -38,6 +41,7 @@ declare @history_data_retention_days int
 declare @history_data_retention_cut_off_date_string varchar(100)
 declare @purgeOperation_ON_OFF char(3)
 declare @lookupName varchar(250)
+declare @error_msg varchar(max)
 
 SET NOCOUNT ON 
 
@@ -53,7 +57,7 @@ END
 declare tableCursor cursor FAST_FORWARD FOR
 
 SELECT  
-	archival_config_id , table_schema,table_name,batch_size,PurgeOnly,filters,source_database_name,destination_database_name,LookupName 
+	archival_config_id , table_schema,table_name,override_batch_size,override_history_data_retention_days,PurgeOnly,filters,source_database_name,destination_database_name,LookupName 
 FROM 
 	dbo.Archival_Config
 where 
@@ -63,7 +67,8 @@ order by
 
 OPEN tableCursor
 FETCH NEXT FROM tableCursor INTO  
-	@archival_config_id,@schemaname,@tablename,@batchsize,@PurgeOnly,@filters,@source_database_name ,@destination_database_name,@lookupName 
+				@archival_config_id,@schemaname,@tablename,@batchsize,@override_history_data_retention_days,@PurgeOnly,@filters,@source_database_name,
+				@destination_database_name,@lookupName 
 
 WHILE @@FETCH_STATUS = 0
           BEGIN
@@ -89,46 +94,58 @@ set @pk_columnname = (select distinct C.COLUMN_NAME FROM
 				   ELSE @tablename END 
 				   	
 				   
-					IF OBJECT_ID(N'tempdb..Staging_IDs', N'U') IS NOT NULL  
-					   DROP TABLE tempdb..Staging_IDs;
+				IF OBJECT_ID(N'tempdb..Staging_IDs', N'U') IS NOT NULL  
+					DROP TABLE tempdb..Staging_IDs;
 					   				 
 
-					  select  @PK_column_dataType =   
-								   ic.DATA_TYPE
-								FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC 
-								INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
-								INNER JOIN INFORMATION_SCHEMA.COLUMNS ic on ku.COLUMN_NAME = ic.COLUMN_NAME
-									ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' 
-									AND TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME 
-									AND KU.table_name=@tablename
-									and ic.TABLE_NAME = @tablename
-								ORDER BY 
-									 KU.TABLE_NAME
-									,KU.ORDINAL_POSITION
+				SELECT  @PK_column_dataType =   
+							ic.DATA_TYPE
+						FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC 
+						INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
+						INNER JOIN INFORMATION_SCHEMA.COLUMNS ic on ku.COLUMN_NAME = ic.COLUMN_NAME
+							ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' 
+							AND TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME 
+							AND KU.table_name=@tablename
+							and ic.TABLE_NAME = @tablename
+						ORDER BY 
+								KU.TABLE_NAME
+							,KU.ORDINAL_POSITION
 									
 						
 						
 						-- If the PK is an INTEGER Column
-						IF @PK_column_dataType = 'int' -- IF PK  is of Integer Data Type
-						BEGIN
-								---- Create a staging table to Hold candidate ids
-								CREATE TABLE tempdb..Staging_IDs(ID int PRIMARY KEY)
-						END
-							ELSE  -- if the PK is a UNIQUEIDENTIFIER
-							BEGIN
-								CREATE TABLE tempdb..Staging_IDs (ID uniqueidentifier PRIMARY KEY);
-							END
+				IF @PK_column_dataType = 'int' -- IF PK  is of Integer Data Type
+				BEGIN
+						---- Create a staging table to Hold candidate ids
+						CREATE TABLE tempdb..Staging_IDs(ID int PRIMARY KEY)
+				END
+				ELSE  -- if the PK is a UNIQUEIDENTIFIER
+					BEGIN
+						CREATE TABLE tempdb..Staging_IDs (ID uniqueidentifier PRIMARY KEY);
+				END
 
-						SET @history_data_retention_days = (select Value from dbo.Lookups where Name = @lookupName)
-						SET @history_data_retention_cut_off_date_string = cast(DATEADD(DAY,-@history_data_retention_days,getdate()) as varchar)
 
-						SET @insert_sql = 'insert into tempdb..Staging_IDs'+
-										' select top ('+cast(@batchsize as varchar)+') '+@pk_columnname + ' from '+ @schemaname+ '.' + @tablename 
+				--section get all the lookup / configuration values  -- STARTS 
+				--SET @history_data_retention_days = (select Value from dbo.Lookups where Name = @lookupName)
+				SET @history_data_retention_days = 
+				(
+					select case when @override_history_data_retention_days is not null then	@override_history_data_retention_days else  
+					(
+						select [Value] from dbo.Lookups where Name = 'ShiftSummaryHistory_Data_Retention_Days' 
+					)end 
+				) 
+				SET @history_data_retention_cut_off_date_string = cast(DATEADD(DAY,-@history_data_retention_days,getdate()) as varchar)
+				SET @batchsize = case when @batchsize is not null then @batchsize else (select Value from dbo.Lookups where Name = 'Batch_Size') end
+				--section get all the lookup / configuration values  -- ENDS
+
+
+				SET @insert_sql = 'insert into tempdb..Staging_IDs'+
+								' select top ('+cast(@batchsize as varchar)+') '+@pk_columnname + ' from '+ @schemaname+ '.' + @tablename 
 									
-						SET @predicate_sql = case when @filters is null then ' where CreatedTime >= (select isnull(max (CreatedTime),''1900-01-01'') from ' +@destination_database_name +'.'++@schemaname+ '.' + @tablearchive + ')'+' order by CreatedTime' 
-								else ' ' + @filters end 
+				SET @predicate_sql = case when @filters is null then ' where CreatedTime >= (select isnull(max (CreatedTime),''1900-01-01'') from '					+@destination_database_name +'.'++@schemaname+ '.' + @tablearchive + ')'+' order by CreatedTime' 
+									else ' ' + @filters end 
 
-						SET @insert_sql = @insert_sql + replace(@predicate_sql,'{Date_Parameter}',''''+@history_data_retention_cut_off_date_string+'''')
+				SET @insert_sql = @insert_sql + replace(@predicate_sql,'{Date_Parameter}',''''+@history_data_retention_cut_off_date_string+'''')
 
 
 							---- Process the records  (Archiving of the records starts here)
@@ -145,16 +162,16 @@ set @pk_columnname = (select distinct C.COLUMN_NAME FROM
 								
 							BEGIN TRY
 								--BEGIN TRAN
-								if @purgeOnly = 0 
+								IF @purgeOnly = 0 
 								BEGIN
 									EXEC dbo.Sp_Arc_Insert_Into_Archive @schemaname,@tablename,@destination_database_name,@filters,@pk_columnname
 								END
-								EXEC dbo.sp_Arc_Delete_from_source @schemaname,@tablename,@filters,@pk_columnname
-								set @numOfRowsAffected = (select count(1) as cnt from tempdb..Staging_IDs)-- Get the number of rows affected
+									EXEC dbo.sp_Arc_Delete_from_source @schemaname,@tablename,@filters,@pk_columnname
+									SET @numOfRowsAffected = (select count(1) as cnt from tempdb..Staging_IDs)-- Get the number of rows affected
 								--COMMIT TRAN
 							END TRY 
 							BEGIN CATCH 
-							declare @error_msg varchar(max)
+							
 								SET @error_msg = 'Error Procedure - ' + ERROR_PROCEDURE() + ' '+ 'Error Line - ' + cast(ERROR_LINE() as varchar) + ' ' + 'Error Message - ' + ERROR_MESSAGE()
 								--ROLLBACK TRAN
 								INSERT INTO dbo.Archival_Error_Log(archival_config_id,error_description,error_date)
@@ -167,8 +184,8 @@ set @pk_columnname = (select distinct C.COLUMN_NAME FROM
 						END -- While Loop End
 					
 			 FETCH NEXT FROM tableCursor 
-				INTO 
-				@archival_config_id,@schemaname,@tablename,@batchsize,@purgeOnly,@filters,@source_database_name ,@destination_database_name,@lookupName 
+				INTO 					@archival_config_id,@schemaname,@tablename,@batchsize,@override_history_data_retention_days,@PurgeOnly,@filters,@source_database_name,
+				@destination_database_name,@lookupName 
 END-- Cursor Loop End
 CLOSE tableCursor
 DEALLOCATE tableCursor				
