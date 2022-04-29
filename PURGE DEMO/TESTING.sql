0b439564cae46ca5b24a5e79550547a7e616f5d0
Arjun Surendra <srao.arjun@gmail.com>
2022-04-29 16:39:47 +0530

    Latest updated Testing Script

--- PURGE DEMO/TESTING.sql
+++ PURGE DEMO/TESTING.sql
@@ -5,7 +5,7 @@ GO
 Need to use  lookup table for global configuration settings 
 a.	Batch Size - (Can be overridden at the archival configuration level)
 b.	Enable Purge - On /Off 
-c.	Purge cut-off Date in Days – Records older than this date to be purged (Can be overridden at the global level)
+c.	Purge cut-off Date in Days – Records older than this date to be purged (Can be overridden at the archival configuration level)
 
 
 Execution Log :
@@ -24,6 +24,7 @@ select
 	lt.Description,lt.Name,lt.Owner
 from 
 	dbo.Lookups l join dbo.LookupTypes lt on l.LookupType_PK_ID = lt.LookupType_PK_ID
+where lt.name Like '%purge%'
 
 select * from dbo.Lookups where name like '%history%'
 
@@ -33,13 +34,20 @@ select * from dbo.Lookups where name like '%history%'
 
 update dbo.Lookups set Value  = 1908 where name = 'ShiftSummaryHistory_Data_Retention_Days'
 
+
+select * from dbo.Lookups where LookupType_PK_ID = 'C77C866E-B81D-4A2C-BFC8-BB253E7CEDFD' and Lookup_PK_ID <> 'A34CA610-82B1-4841-A590-EAE64B97EBCB'
+
 select 
-	archival_config_id,table_schema,table_name,batch_size,PurgeOnly,filters,is_enabled,db_datetime_last_updated from dbo.Archival_Config 
+	archival_config_id,table_schema,table_name,override_batch_size,override_history_data_retention_days,PurgeOnly,filters,is_enabled,db_datetime_last_updated from dbo.Archival_Config 
 where 
 	is_enabled = 1
 
 
 
+	select * from dbo.Lookups where Name = 'ShiftSummaryHistory_Data_Retention_Days' 
+
+	
+	
 
 select count(1) as cnt from dbo.ShiftSummaryDisplayHistory with (nolock) -- DBO (schema)
 
@@ -51,10 +59,12 @@ select count(1) as cnt from dbo.ShiftSummaryHistory with (nolock) ---
 
 select count(1) as cnt from dbo.ShiftSummaryHistory with (nolock) where Shift_EndTime < DATEADD(day,-1908,getdate())
 
-
+select count(1) as cnt from dbo.ShiftSummaryHistory with (nolock) where Shift_EndTime < DATEADD(day,-1940,getdate())
 
 begin tran
 exec dbo.Sp_Arc_RunArchiving
+
+
 
 rollback tran
 
@@ -65,30 +75,17 @@ exec sp_whoisactive
 select * from dbo.Archival_Config where is_enabled = 1 
 
 
+
+
+
+
 select * from dbo.Lookups
 
 
 
-update dbo.Archival_Config set is_enabled = 0 where archival_config_id <> 6
 
 
 
-begin tran 
-
-
-/*
-Use the lookup table for global configuration 
-
-Purging date criteria - At global level
-Enabled/Disabled : On/OFF
-
-Number of records purged and its duration (Execution log)
-
-batch size in lookups 
-
-Need to decide which settings can be overriden at the archival configuration level from global level
-
-*/
 
 
 select * from [dbo].[ShiftSummaryDisplayHistory]
@@ -111,10 +108,51 @@ select
 
 
 
-	
-insert into tempdb..Staging_IDs 
 
-select * from tempdb..Staging_IDs 
+	USE [Honeywell.MES.Operations.DataModel.OperationsDB]
+GO
 
 
-select top (1000) ShiftSummaryHistory_PK_ID from dbo.ShiftSummaryHistory  where Shift_EndTime < 'Jan  3 2017  7:44PM'
+select * from dbo.Lookups where LookupType_PK_ID = 'C77C866E-B81D-4A2C-BFC8-BB253E7CEDFD'
+
+select 
+	archival_config_id,table_schema,table_name,override_batch_size,override_history_data_retention_days,PurgeOnly,filters,is_enabled,db_datetime_last_updated from dbo.Archival_Config 
+where 
+	is_enabled = 1
+
+select count(1) as cnt from dbo.ShiftSummaryDisplayHistory with (nolock) -- DBO (schema)
+
+select count(1) as cnt from dbo.ShiftSummaryHistory with (nolock) where Shift_EndTime < DATEADD(day,-1908,getdate()) --30043
+
+select count(1) as cnt from dbo.ShiftSummaryHistory with (nolock) where Shift_EndTime < DATEADD(day,-1940,getdate()) --5520
+
+
+begin tran
+exec dbo.Sp_Arc_RunArchiving
+
+
+
+rollback tran
+
+
+
+
+select * from dbo.Archival_Execution_Log with (nolock)
+
+update dbo.Archival_Config set override_batch_size = null where is_enabled = 1 
+update dbo.Archival_Config set override_history_data_retention_days = null where is_enabled = 1 
+
+
+
+
+select * from dbo.Archival_Execution_Log
+
+
+select override_batch_size , override_history_data_retention_days from dbo.Archival_Config where is_enabled = 1 
+
+
+
+select * from dbo.Archival_Execution_Log
+
+
+
